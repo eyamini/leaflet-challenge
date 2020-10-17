c98618afb2bbc05eefd85535c4a4187f66aa1ee2
Esaa Yamini <63874540+eyamini@users.noreply.github.com>
2020-10-17 12:52:12 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,7 @@
+//Set USGS query URL
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-
 d3.json(queryUrl, function(data) {
-
   createFeatures(data.features);
   console.log(data.features)
 });
@@ -13,33 +12,29 @@ function createFeatures(earthquakeData) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
-
   function radiusSize(magnitude) {
-    return magnitude * 20000;
+    return magnitude * 18000;
   }
-
-
   function circleColor(magnitude) {
     if (magnitude < 1) {
-      return "#ccff33"
+      return "#4d976b"
     }
     else if (magnitude < 2) {
-      return "#ffff33"
+      return "#6dbd64"
     }
     else if (magnitude < 3) {
-      return "#ffcc33"
+      return "#70a683"
     }
     else if (magnitude < 4) {
-      return "#ff9933"
+      return "#4d99ac"
     }
     else if (magnitude < 5) {
-      return "#ff6633"
+      return "#79b1c4"
     }
     else {
-      return "#ff3333"
+      return "#948b53"
     }
   }
-
 
   var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: function(earthquakeData, latlng) {
@@ -56,7 +51,6 @@ function createFeatures(earthquakeData) {
 }
 
 function createMap(earthquakes) {
-
 
   var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -79,61 +73,34 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Create the faultline layer
-  var faultLine = new L.LayerGroup();
-  
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Outdoor Map": outdoorsmap,
-    "Greyscale Map": grayscalemap,
-    "Satellite Map": satellitemap
-  };
-
-  // Create overlay object to hold our overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes,
-    FaultLines: faultLine
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Map with earthquake layers
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 4,
-    layers: [outdoorsmap, earthquakes, faultLine]
+    layers: [outdoorsmap, earthquakes]
   });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
-  // Query to retrieve the faultline data
-  var faultlinequery = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
-  
-  // Create the faultlines and add them to the faultline layer
-  d3.json(faultlinequery, function(data) {
-    L.geoJSON(data, {
-      style: function() {
-        return {color: "orange", fillOpacity: 0}
-      }
-    }).addTo(faultLine)
-  })
-
-  // color function to be used when creating the legend
+  //Legend Color
   function getColor(d) {
-    return d > 5 ? '#ff3333' :
-           d > 4  ? '#ff6633' :
-           d > 3  ? '#ff9933' :
-           d > 2  ? '#ffcc33' :
-           d > 1  ? '#ffff33' :
-                    '#ccff33';
+    return d > 5 ? '#6dbd64' :
+           d > 4  ? '#6dbd64' :
+           d > 3  ? '#70a683' :
+           d > 2  ? '#4d99ac' :
+           d > 1  ? '#79b1c4' :
+                    '#948b53';
   }
 
-// Add legend to the map
+//Insert legend
   var legend = L.control({position: 'bottomright'});
   
   legend.onAdd = function (map) {
@@ -142,7 +109,6 @@ function createMap(earthquakes) {
           mags = [0, 1, 2, 3, 4, 5],
           labels = [];
 
-          
       for (var i = 0; i < mags.length; i++) {
           div.innerHTML +=
               '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
